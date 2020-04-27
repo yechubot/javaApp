1b3b1bb213dc7133971a12254b4009c8186c829b
yechubot <61838900+yechubot@users.noreply.github.com>
2020-04-27 23:43:34 +0900

    Update savingApp.java

--- Apptwo/src/savingApp.java
+++ Apptwo/src/savingApp.java
@@ -5,15 +5,18 @@ public class savingApp {
 		double monthlyIncome = 1500; // dollars
 		double moneyFood = 200;
 		double moneyBus = 100;
+		double moneyRent = 300;
 		double totalSaving = monthlyIncome - (moneyFood + moneyBus);
 		double savingForFood = totalSaving * 0.3;
 		double savingForBus = totalSaving * 0.2;
 		double savingInAccount = totalSaving * 0.5;
+		double deposit = moneyRent *10;
 		
 		System.out.println("total Saving : " + totalSaving);
 		System.out.println("saving for food : " + savingForFood);
 		System.out.println("saving for bus : " + savingForBus);
 		System.out.println("saving in account : " + savingInAccount);
+		System.out.println("deposit : " + deposit);
 		
 	}
 	
